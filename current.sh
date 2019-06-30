d6e9519858d6d01137fd25a21e913086bc57df2d
Balla Roland <balla.roland96@gmail.com>
2019-06-30 11:17:17 +0300

    user, date

---
+++ current.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function line(){
+	echo "*************************************************"
+}
+
+echo "Your username : $(echo $USER)"
+line
+
+echo "Current date and time : $(date)"
+line
+
+echo "Currently logged on users:"
+who
+line
